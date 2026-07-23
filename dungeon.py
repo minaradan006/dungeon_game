ba7d41eb31b55c591708df329f58e39d0ce95093
minaradan006 <minaradan006@gmail.com>
2026-07-23 17:46:16 +0300

    add scenarios 1 and 2

--- dungeon.py
+++ dungeon.py
@@ -2,82 +2,8 @@ import random
 import time
 import textwrap
 from obj import Weapon, Armour, Potion, Character, Room
-from items import walls
 from story import show_start_screen, begin_story
-
-def print_choices(valid_choices):
-	for choice in valid_choices:
-		print(f"[{choice}]", end="  ")
-		time.sleep(0.5)
-	print()
-
-def inventory(character: Character):
-	print("INVENTORY".center(40, "=") + "\n")
-
-	for category, content in character.inventory.items():
-		print(f"{category.title()}".center(20, "-") + "\n")
-
-		for item in content:
-			print(item)
-
-		print()
-		time.sleep(0.5)
-
-	while(True):
-		print("What do you want to do?")
-		time.sleep(0.5)
-		choices = ["Equip Weapon", "Equip Armour", "Heal", "Go back"]
-		print_choices(choices)
-
-		choice = input()
-		print()
-
-		if choice.lower() == "equip weapon":
-			weapon_name = input("What weapon do you want to equip? ")
-			print()
-			character.equip_weapon(weapon_name)
-
-		if choice.lower() == "equip armour":
-			armour_name = input("What armour do you want to equip? ")
-			print()
-			character.equip_armour(armour_name)
-
-		if choice.lower() == "heal":
-			potion_name = input("What potion do you want to use? ")
-			print()
-			character.heal(potion_name)
-
-		if choice.lower() == "go back":
-			break
-
-	print("Closing inventory..." + "\n")
-
-def scenario_1(character: Character, level: int):
-	print(textwrap.fill(f"A wall of {random.choice(walls)} blocks your sides. You can only go [ahead].", width=60) + "\n")
-	time.sleep(3)
-
-	ahead = Room(level)
-	print(textwrap.fill(f"Ahead, there is {ahead.desc}({ahead.event}).", width=70) + "\n")
-	time.sleep(3)
-
-	choices = ["Go ahead", "Check stats", "Open inventory"]
-
-	while True:
-		print("What will you do?")
-		time.sleep(0.5)
-		print_choices(choices)
-		choice = input()
-		print()
-
-		if choice.lower() == "check stats":
-			print("STATS".center(40, "=") + "\n")
-			print(character)
-
-		if choice.lower() == "open inventory":
-			inventory(character)
-
-		if choice.lower() == "go ahead":
-			return ahead
+from scenarios  import scenario_1, scenario_2, scenario_3
 
 if __name__ == "__main__":
 	show_start_screen()
@@ -103,11 +29,13 @@ if __name__ == "__main__":
 		mini_stages = random.choice(range(1, 2))
 
 		for mini in range(0, mini_stages):
-			scenario = random.choice(range(1, 2))
+			scenario = random.choice([1, 2, 3])
 
 			if scenario == 1:
-				choice = scenario_1(character, level)
+				room = scenario_1(character, level)
 			elif scenario == 2:
-				pass
+				room =  scenario_2(character, level)
+			elif scenario == 3:
+				room = scenario_3(character, level)
 
 	print("You did it!")
---
+++ scenarios.py
@@ -0,0 +1,159 @@
+import time
+import textwrap
+import random
+from obj import Character, Room
+from items import walls, room_adjectives
+
+def print_choices(valid_choices):
+	for choice in valid_choices:
+		print(f"[{choice}]", end="  ")
+	print()
+
+def inventory(character: Character):
+	print("INVENTORY".center(40, "=") + "\n")
+
+	for category, content in character.inventory.items():
+		print(f"{category.title()}".center(20, "-") + "\n")
+
+		for item in content:
+			print(item)
+
+		print()
+		time.sleep(0.5)
+
+	while(True):
+		print("What do you want to do?")
+		time.sleep(0.5)
+		choices = ["Equip Weapon", "Equip Armour", "Heal", "Go back"]
+		print_choices(choices)
+
+		choice = input()
+		print()
+
+		if choice.lower() == "equip weapon":
+			weapon_name = input("What weapon do you want to equip? ")
+			print()
+			character.equip_weapon(weapon_name)
+
+		if choice.lower() == "equip armour":
+			armour_name = input("What armour do you want to equip? ")
+			print()
+			character.equip_armour(armour_name)
+
+		if choice.lower() == "heal":
+			potion_name = input("What potion do you want to use? ")
+			print()
+			character.heal(potion_name)
+
+		if choice.lower() == "go back":
+			break
+
+	print("Closing inventory..." + "\n")
+
+def scenario_1(character: Character, level: int):
+	print(textwrap.fill(f"Walls of {random.choice(walls)} block your sides. You can only go [ahead].", width=70) + "\n")
+	time.sleep(3)
+
+	ahead = Room(level)
+	print(textwrap.fill(f"Ahead, there is {ahead.desc}({ahead.event}).", width=70) + "\n")
+	time.sleep(3)
+
+	choices = ["Go ahead", "Check stats", "Open inventory"]
+
+	while True:
+		print("What will you do?")
+		time.sleep(0.5)
+		print_choices(choices)
+		choice = input()
+		print()
+
+		if choice.lower() == "check stats":
+			print("STATS".center(40, "=") + "\n")
+			print(character)
+
+		if choice.lower() == "open inventory":
+			inventory(character)
+
+		if choice.lower() == "go ahead":
+			print("You decide to go straight.\n")
+			return ahead
+
+def scenario_2(character: Character, level: int):
+	print(textwrap.fill(f"A wall of {random.choice(walls)} blocks your path. You can only go [left] or [right].", width=70) + "\n")
+	time.sleep(3)
+
+	left = Room(level)
+	print(textwrap.fill(f"To the left, there is {left.desc}({left.event}).", width=70) + "\n")
+	time.sleep(3)
+
+	right = Room(level)
+	print(textwrap.fill(f"To the right, there is {right.desc}({right.event}).", width=70) + "\n")
+	time.sleep(3)
+
+	choices = ["Go left", "Go right", "Check stats", "Open inventory"]
+
+	while True:
+		print("What will you do?")
+		time.sleep(0.5)
+		print_choices(choices)
+		choice = input()
+		print()
+	
+		if choice.lower() == "check stats":
+			print("STATS".center(40, "=") + "\n")
+			print(character)
+	
+		if choice.lower() == "open inventory":
+			inventory(character)
+	
+		if choice.lower() == "go left":
+			print("You decide to go to the left.\n")
+			return left
+
+		if choice.lower() == "go right":
+			print("You decide to go to the right.\n")
+			return right
+
+def scenario_3(character: Character, level: int):
+	print(textwrap.fill(f"You stand at a crossroad. You can go [left], [right] or [ahead].", width=70) + "\n")
+	time.sleep(3)
+
+	left = Room(level)
+	print(textwrap.fill(f"To the left, there is {left.desc}({left.event}).", width=70) + "\n")
+	time.sleep(3)
+
+	right = Room(level)
+	print(textwrap.fill(f"To the right, there is {right.desc}({right.event}).", width=70) + "\n")
+	time.sleep(3)
+
+	ahead = Room(level)
+	print(textwrap.fill(f"Ahead, there is {ahead.desc}({ahead.event}).", width=70) + "\n")
+	time.sleep(3)
+
+	choices = ["Go left", "Go right","Go ahead", "Check stats", "Open inventory"]
+
+	while True:
+		print("What will you do?")
+		time.sleep(0.5)
+		print_choices(choices)
+		choice = input()
+		print()
+	
+		if choice.lower() == "check stats":
+			print("STATS".center(40, "=") + "\n")
+			print(character)
+	
+		if choice.lower() == "open inventory":
+			inventory(character)
+	
+		if choice.lower() == "go left":
+			print("You decide to go to the left.\n")
+			return left
+
+		if choice.lower() == "go right":
+			print("You decide to go to the right.\n")
+			return right
+
+		if choice.lower() == "go ahead":
+			print("You decide to go straight.\n")
+			return ahead
